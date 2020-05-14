ff734e956836c9d989ecec79fd7a392cb15c4a88
Chris Allen <chrisallen415@gmail.com>
2020-05-14 20:36:13 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
   if number % 15 == 0
-    puts "FizzBuzz"
+    "FizzBuzz"
   elsif number % 5 == 0
-    puts "Buzz"
+    "Buzz"
   elsif number % 3 == 0
-    puts "Fizz"
+    "Fizz"
   else
-    puts number
+    "nil"
 end

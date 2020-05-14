b82f41abe48ec286acf309336cc338b0f68f38a8
Chris Allen <chrisallen415@gmail.com>
2020-05-14 20:54:50 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,9 +3,11 @@
 def fizzbuzz(number)
   if number % 3 == 0
     "Fizz"
-  elsif number % 5 == 0
+  elsif
+    number % 5 == 0
     "Buzz"
-  elsif number % 15 == 0
+  elsif
+    number % 15 == 0
     "FizzBuzz"
   else
     "nil"
